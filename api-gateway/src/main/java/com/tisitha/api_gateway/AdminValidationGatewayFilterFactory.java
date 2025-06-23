58eae81a30e3bc7ee267ba20d273e1cdbca681b6
Tisitha <tisitha007@gmail.com>
2025-06-23 23:23:02 +0530

    update api-gateway

---
+++ api-gateway/src/main/java/com/tisitha/api_gateway/AdminValidationGatewayFilterFactory.java
@@ -0,0 +1,38 @@
+package com.tisitha.api_gateway;
+
+import org.apache.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class AdminValidationGatewayFilterFactory extends AbstractGatewayFilterFactory<Object> {
+
+    private final WebClient webClient;
+
+    public AdminValidationGatewayFilterFactory(WebClient.Builder webClientBuilder, @Value("${user.service.url}") String authServiceUrl) {
+        this.webClient = webClientBuilder.baseUrl(authServiceUrl).build();
+    }
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) ->{
+            String token= exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+            if(token==null || !token.startsWith("Bearer ")){
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+            return webClient.get()
+                    .uri("/validate-admin")
+                    .header(HttpHeaders.AUTHORIZATION,token)
+                    .retrieve()
+                    .toBodilessEntity()
+                    .then(chain.filter(exchange));
+        };
+    }
+
+}
+
