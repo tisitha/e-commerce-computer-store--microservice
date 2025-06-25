a4a9be59717140dba65d730721645f55e3059cfa
Tisitha <tisitha007@gmail.com>
2025-06-25 07:02:50 +0530

    add api-docs to order-service

--- order-service/src/main/java/com/tisitha/order_service/controller/CartItemController.java
+++ order-service/src/main/java/com/tisitha/order_service/controller/CartItemController.java
@@ -3,6 +3,7 @@ package com.tisitha.order_service.controller;
 import com.tisitha.order_service.dto.CartItemRequestDTO;
 import com.tisitha.order_service.dto.CartItemResponseDTO;
 import com.tisitha.order_service.service.CartItemService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,22 +21,26 @@ public class CartItemController {
         this.cartItemService = cartItemService;
     }
 
+    @Operation(summary = "Get cart items of a user")
     @GetMapping("/user/{id}")
     public ResponseEntity<List<CartItemResponseDTO>> getCartItems(@PathVariable("id") UUID id) {
         return new ResponseEntity<>(cartItemService.getCartItems(id), HttpStatus.OK);
     }
 
+    @Operation(summary = "Add item to cart")
     @PostMapping("/add")
     public ResponseEntity<CartItemResponseDTO> addCartItem(@RequestBody CartItemRequestDTO dto) {
         return new ResponseEntity<>(cartItemService.addCart(dto), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Delete item from cart")
     @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteCartItem(@PathVariable("id") UUID id) {
         cartItemService.deleteCartItem(id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @Operation(summary = "Update the quantity of a cart item")
     @PutMapping("/update")
     public ResponseEntity<?> updateCartItem(@RequestParam("uid") UUID id, @RequestParam Integer quantity) {
         cartItemService.updateCartItem(id, quantity);
--- order-service/src/main/java/com/tisitha/order_service/controller/OrderController.java
+++ order-service/src/main/java/com/tisitha/order_service/controller/OrderController.java
@@ -4,6 +4,7 @@ import com.tisitha.order_service.dto.CartItemRequestDTO;
 import com.tisitha.order_service.dto.OrderGetRequestDTO;
 import com.tisitha.order_service.dto.OrderResponseDTO;
 import com.tisitha.order_service.service.OrderService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,22 +22,26 @@ public class OrderController {
         this.orderService = orderService;
     }
 
+    @Operation(summary = "Get all orders")
     @GetMapping
     public ResponseEntity<OrderResponseDTO> getOrders(@RequestBody OrderGetRequestDTO requestDTO){
         return new ResponseEntity<>(orderService.getOrderData(requestDTO), HttpStatus.OK);
     }
 
+    @Operation(summary = "Get orders of a user")
     @GetMapping("/{id}")
     public ResponseEntity<OrderResponseDTO> getOrdersByCustomer(@PathVariable UUID id,@RequestBody OrderGetRequestDTO requestDTO){
         return new ResponseEntity<>(orderService.getOrdersByCustomer(id,requestDTO), HttpStatus.OK);
     }
 
+    @Operation(summary = "Add new order")
     @PostMapping
     public ResponseEntity<Void> addOrders(@RequestHeader("Authorization") String authHeader,@RequestBody List<CartItemRequestDTO> dtos){
         orderService.addOrder(authHeader,dtos);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Delete a order")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteOrderList(@PathVariable UUID id){
         orderService.deleteOrderData(id);
