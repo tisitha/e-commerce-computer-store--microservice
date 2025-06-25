b5f003e1bd20962f2c723e701c86d0b1c47c2624
Tisitha <tisitha007@gmail.com>
2025-06-25 07:05:35 +0530

    add api-docs to inventory-service

--- inventory-service/src/main/java/com/tisitha/inventory_service/controller/InventoryController.java
+++ inventory-service/src/main/java/com/tisitha/inventory_service/controller/InventoryController.java
@@ -2,6 +2,7 @@ package com.tisitha.inventory_service.controller;
 
 import com.tisitha.inventory_service.dto.InventoryDTO;
 import com.tisitha.inventory_service.service.InventoryService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,21 +19,25 @@ public class InventoryController {
         this.inventoryService = inventoryService;
     }
 
+    @Operation(summary = "Get quantity of a product")
     @GetMapping("get-quantity/{id}")
     public ResponseEntity<InventoryDTO> getQuantity(@PathVariable UUID id){
         return new ResponseEntity<>(inventoryService.getQuantity(id), HttpStatus.OK);
     }
 
+    @Operation(summary = "Add a quantity data of a product")
     @PostMapping("add-quantity/{id}")
     public ResponseEntity<InventoryDTO> addQuantity(@PathVariable UUID id,@RequestParam Integer quantity){
         return new ResponseEntity<>(inventoryService.addQuantity(id,quantity), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Update quantity of a product")
     @PutMapping("update-quantity/{id}")
     public ResponseEntity<InventoryDTO> updateQuantity(@PathVariable UUID id,@RequestParam Integer quantity,@RequestParam String productName){
         return new ResponseEntity<>(inventoryService.updateQuantity(id,quantity,productName), HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Delete quantity data of a product")
     @DeleteMapping("delete-quantity/{id}")
     public ResponseEntity<Void> deleteQuantity(@PathVariable UUID id){
         inventoryService.deleteFromInventory(id);
