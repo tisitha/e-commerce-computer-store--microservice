f7c8145de4e2200ddd36b3af051be660ba497dba
Tisitha <tisitha007@gmail.com>
2025-06-25 06:57:28 +0530

    fix user-service

--- user-service/src/main/java/com/tisitha/user_service/controller/AuthController.java
+++ user-service/src/main/java/com/tisitha/user_service/controller/AuthController.java
@@ -4,7 +4,6 @@ import com.tisitha.user_service.dto.*;
 import com.tisitha.user_service.service.AuthService;
 import com.tisitha.user_service.service.ForgotPasswordService;
 import io.swagger.v3.oas.annotations.Operation;
-import org.hibernate.sql.ast.tree.expression.Summarization;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
