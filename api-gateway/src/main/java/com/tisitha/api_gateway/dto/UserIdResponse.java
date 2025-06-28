6787719af8aca887211d625c55b49a68345c7a7d
Tisitha <tisitha007@gmail.com>
2025-06-28 17:26:26 +0530

    updated api-gateway

---
+++ api-gateway/src/main/java/com/tisitha/api_gateway/dto/UserIdResponse.java
@@ -0,0 +1,13 @@
+package com.tisitha.api_gateway.dto;
+
+public class UserIdResponse {
+    private String userId;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+}
